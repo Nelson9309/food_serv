929e939e47b7fcf753147724aa5ea3ab0867b4b1
Nelson9309 <chemist144@gmail.com>
2020-05-28 01:46:52 +0300

    123

--- script.js
+++ script.js
@@ -3,5 +3,32 @@ $(document).ready(function() {
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    $(document).ready(function(){
+        $('.slider').slick({
+            arrows:true,
+            dots:true,
+            slidesToShow:3,
+            autoplay:true,
+            speed:1000,
+            autoplaySpeed:800,
+            responsive:[
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow:2
+                    }
+                },
+                {
+                    breakpoint: 550,
+                    settings: {
+                        slidesToShow:1
+                    }
+                }
+            ]
+        });
+    });
+    
+    
 });
 
